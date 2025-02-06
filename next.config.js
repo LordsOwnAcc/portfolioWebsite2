aed58eaa9d699fa7fcafd145922b1254f12ae8f0
LordsOwnAcc <sumity12019@gmail.com>
2025-02-06 20:25:02 +0530

    added config

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images: { unoptimized: true },
+    basePath: "/portfolioWebsite2",  // Your repository name
+    assetPrefix: "/portfolioWebsite2",
+  };
+  
+  module.exports = nextConfig;
