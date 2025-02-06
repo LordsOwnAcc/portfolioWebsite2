329df48d6ed5e4b459328afc5b160f8693a2c21a
LordsOwnAcc <sumity12019@gmail.com>
2025-02-06 20:47:10 +0530

    ch

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 const nextConfig = {
     output: 'export',
     images: { unoptimized: true },
-    basePath: "/portfolioWebsite2",  // Your repository name
-    assetPrefix: "/portfolioWebsite2",
+    basePath: "/portfolioWebsite2",  // Must match your repo name exactly
+    assetPrefix: "/portfolioWebsite2/",
+    trailingSlash: true, // Important for GitHub Pages
   };
   
   module.exports = nextConfig;
+  
