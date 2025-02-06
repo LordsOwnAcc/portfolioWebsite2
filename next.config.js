309cba99eee836156cdee7b798ef9bec105445fd
LordsOwnAcc <sumity12019@gmail.com>
2025-02-06 20:36:21 +0530

    Save local changes before pulling

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: 'export',
     images: { unoptimized: true },
-    basePath: "/portfolioWebsite2",  
+    basePath: "/portfolioWebsite2",  // Your repository name
     assetPrefix: "/portfolioWebsite2",
   };
   
